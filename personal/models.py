748870eec72019a123a0d7b1170cea5125ff9b96
AshantC <ashantchaudhary22@gmail.com>
2024-06-30 00:46:43 +0545

    making dynamic content

--- personal/models.py
+++ personal/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100, blank=True)
+    message = models.TextField()
+    
+    def __str__(self):
+        return self.name
+    
+class Review(models.Model):
+    name = models.CharField(max_length=100)
+    post = models.CharField(max_length=400)
+    image = models.TextField(blank=True)
+    review = models.TextField()
+    
+    def __str__(self):
+        return f"{self.name}  {self.post}"
+    
+    
--- personal/views.py
+++ personal/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render
+from personal.models import Contact, Review
 
 # Create your views here.
 
 def home(request):
-    return render(request, "index.html")
+    views = {}
+    views["reviews"] = Review.objects.all()
+    return render(request, "index.html", views)
 
 def about(request):
     return render(request, "about.html")
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        email = request.POST["email"]
+        subject = request.POST["subject"]
+        message = request.POST["message"]
+        
+        data = Contact.objects.create(
+            name = name,
+            email = email,
+            subject = subject,
+            message = message
+        )
+        data.save()
+        # this message is for notification purpose
+        message = {"msg": "Your message has been sent!"}
+        return render(request, "contact.html", message)
+        
     return render(request, "contact.html")
     
 def portfolio(request):
