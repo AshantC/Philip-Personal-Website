25b64ab8192fa4d5b7b8c1dc55d9b16e0a73fa82
AshantC <ashantchaudhary22@gmail.com>
2024-06-29 15:55:09 +0545

    template images and links fixed

--- personal/urls.py
+++ personal/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from personal.views import home
+from personal.views import home, about, contact, portfolio, price, services
 urlpatterns = [
     path('', home, name='home'),
+    path('about', about, name='about'),
+    path('contact', contact, name='contact'),
+    path('portfolio', portfolio, name='portfolio'),
+    path('price', price, name='price'),
+    path('services', services, name='services'),
 ]
+ 
